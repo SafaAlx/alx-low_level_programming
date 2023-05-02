ebdaf9f03e71474db97f9f3c57132cdfe68faa76
SafaAlx <safae.mimoune@gmail.com>
2023-05-02 14:31:22 -0700

    7-puts_half.c

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -7,17 +7,29 @@
 */
 
 void puts_half(char *str)
+
 {
-	int i;
 
-	for (i = 0; str[i] != '\0'; i++)
-		;
+	int j = 0;
+	int k;
 
-	i++;
-	for (i /= 2; str[i] != '\0'; i++)
+	while (str[j] != '\0')
 	{
-		if (i % 2 == 0)
-			putchar(str[i]);
+		j++;
+	}
+	if (j % 2 == 1)
+	{
+		k = (j - 1) / 2;
+		k += 1;
+	}
+	else
+	{
+	k = j / 2;
+	}
+
+	for (; k < j; k++)
+	{
+		putchar(str[k]);
 	}
 	putchar('\n');
 }
