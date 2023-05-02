3e518aceaeb3cf1b35d09a249329a52b5cb15bc4
SafaAlx <safae.mimoune@gmail.com>
2023-05-02 13:51:44 -0700

    7-puts_half.c

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * puts_half - prints every character of a string
+ * @str: char array string type
+ * Description: if odd number of chars, print (length - 1) / 2
+*/
+
+void puts_half(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+		;
+
+	i++;
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		if (i % 2 == 0)
+			putchar(str[i]);
+	}
+	putchar('\n');
+}
