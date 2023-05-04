9ff5ce624c4c4f155483aa6b3d79b1b8e0dc4aa8
SafaAlx <safae.mimoune@gmail.com>
2023-05-04 04:47:09 -0700

    0-strcat.c

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -4,6 +4,7 @@
  * _strcat - concatenates two strings
  * @dest: input value
  * @src: input value
+ *
  * return: void
 */
 char *_strcat(char *dest, char *src)
@@ -23,6 +24,7 @@ char *_strcat(char *dest, char *src)
 		i++;
 		j++;
 	}
+
 	dest[i] = '\0';
 	return (dest);
 }
