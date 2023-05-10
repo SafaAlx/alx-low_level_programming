4dd8e478bb9d013e2dc89485f2f154e83c68cf38
SafaAlx <safae.mimoune@gmail.com>
2023-05-10 04:30:22 -0700

    1-print_rev_recursion.c

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -6,15 +6,15 @@
  *
  * Return: Nothing.
 */
-void _puts_recursion(char *s)
+void _print_rev_recursion(char *s)
 {
 	if (*s == '\0')
 	{
-		_putchar('\n');
 		return;
 	}
 
-	_putchar(*s);
 	s++;
-	_puts_recursion(s);
+	_print_rev_recursion(s);
+	s--;
+	_putchar(*s);
 }
