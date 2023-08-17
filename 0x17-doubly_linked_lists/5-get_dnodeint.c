7824a6e1d6382a6503b5907c5bb925c2758630d3
SafaAlx <safae.mimoune@gmail.com>
2023-08-17 14:29:55 -0700

    lists.h

--- 0x17-doubly_linked_lists/5-get_dnodeint.c
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -6,7 +6,7 @@
  * @index: index of the node to search for, starting from 0
  * Return: nth node or null
  */
-dlistint_t *get_dnodeint_at_index(dlistint_t **head, unsigned int index)
+dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 {
 	unsigned int size;
 	dlistint_t *tmp;
