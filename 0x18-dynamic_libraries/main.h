78551a0b9942036d3e8153d85ae2b3f370143fff
SafaAlx <safae.mimoune@gmail.com>
2023-08-21 16:16:57 -0700

    0x18-dynamic_libraries

---
+++ 0x18-dynamic_libraries/main.h
@@ -0,0 +1,21 @@
+#ifndef _MAIN_H_
+#define _MAIN_H_
+
+int _putchar(char c);
+int _isupper(int c);
+char *_memset(char *s, char b, unsigned int n);
+char *_strcat(char *dest, char *src);
+int _isdigit(int x);
+char *_memcpy(char *dest, char *src, unsigned int n);
+char *_strchr(char *s, char c);
+int _strlen(char *s);
+int _islower(int C);
+void _puts(char *str);
+unsigned int _strspn(char *s, char *accept);
+int _isalpha(int c);
+char *_strpbrk(char *s, char *accept);
+char *_strstr(char *haystack, char *needle);
+int _abs(int n);
+char *_strcpy(char *dest, char *src);
+
+#endif /* _MAIN_H_ */
